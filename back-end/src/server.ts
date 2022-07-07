849a05c7ad937061ba9c374869fdabcb93fa0e30
Felipe Matheus <fmatheusog@gmail.com>
2022-07-06 22:30:32 -0300

    feat(database): create docker-compose mongodb service and server db connection

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,12 +1,14 @@
 import * as dotenv from 'dotenv';
 import App from './app';
+import MongoConnection from './repositories/implementations/mongodb/mongo.connection';
 
 dotenv.config();
 
 async function main() {
   try {
-    const { SERVER_PORT } = process.env;
+    const { SERVER_PORT, DB_HOST } = process.env;
     const app = new App();
+    await MongoConnection.connect(`mongodb://${DB_HOST}:/todo_db`);
     app.start(SERVER_PORT);
   } catch(error) {
     console.log(error);
